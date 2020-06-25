0676bc553ff6164a13b7e515afa4099345458ce5
johnWZ <johnzhaiisgreat@gmail.com>
2020-06-25 14:30:34 +0930

    update

--- Udemy_webBootcamp/Todo-List-Project-FA5-edition/assets/js/todos.js
+++ Udemy_webBootcamp/Todo-List-Project-FA5-edition/assets/js/todos.js
@@ -6,15 +6,19 @@ $("ul").on("click", "li", function(){
 //Click on X to delete Todo
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
+		// this this is this.parent()
 		$(this).remove();
 	});
+	// stop triger its parent's event
 	event.stopPropagation();
 });
 
 $("input[type='text']").keypress(function(event){
+	// event.which records the key pressed, 13 means enter key
 	if(event.which === 13){
 		//grabbing new todo text from input
 		var todoText = $(this).val();
+		// clear the input field
 		$(this).val("");
 		//create a new li and add to ul
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
--- Udemy_webBootcamp/jqueryQuiz1.js
+++ Udemy_webBootcamp/jqueryQuiz1.js
@@ -4,4 +4,4 @@ $("div.highlight").css("width","200px");
 
 $("div#third").css("border","2px solid orange");
 
-$("div:first-of-type").css("color","pink");
+$("div").first().css("color","pink");
