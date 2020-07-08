130607964984db7ac0731fc04623b85e7095194e
johnWZ <johnzhaiisgreat@gmail.com>
2020-07-08 23:59:07 +0930

    present todo list data

--- express-todolist/controllers/todoController.js
+++ express-todolist/controllers/todoController.js
@@ -1,7 +1,10 @@
-// 将它导出
+// 设置一组todo数据
+var data = [{item: 'first todo'}, {item: 'second todo'}, {item: 'third todo'}];
+// 将下面的模块导出
 module.exports = function(app) {
     app.get('/todo', function(req, res){
-        res.render('todo');
+        // 将todo数据渲染出来
+        res.render('todo', {todos: data});
     });
 
     app.post('/todo', function(req, res){
