f94b5ba4d4bda17682a2642357265781d72b5406
johnWZ <johnzhaiisgreat@gmail.com>
2020-07-09 10:45:47 +0930

    update

--- express-todolist/controllers/todoController.js
+++ express-todolist/controllers/todoController.js
@@ -1,4 +1,8 @@
-// 设置一组todo数据
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended:false });
+
+// 设置一组todo数组
 var data = [{item: 'first todo'}, {item: 'second todo'}, {item: 'third todo'}];
 // 将下面的模块导出
 module.exports = function(app) {
@@ -7,12 +11,19 @@ module.exports = function(app) {
         res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function(req, res){
-        //
+    app.post('/todo', urlencodedParser, function(req, res){
+        // 将req.body里的内容push到todo数组中去
+        data.push(req.body);
+        // 响应一下json防止报错，响应的内容无所谓
+        res.json(data);
     });
 
-    app.delete('/todo', function(req, res){
+    app.delete('/todo/:item', function(req, res){
         //
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 }
 
