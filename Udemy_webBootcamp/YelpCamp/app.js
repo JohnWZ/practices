141906c7d7de33e71d9d69c16e948c10b6390688
johnWZ <johnzhaiisgreat@gmail.com>
2020-07-21 20:33:21 +0930

    add campground

--- Udemy_webBootcamp/YelpCamp/app.js
+++ Udemy_webBootcamp/YelpCamp/app.js
@@ -1,3 +1,21 @@
 var express = require("express");
 var app = express();
+app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "position 1", image: ""},
+        {name: "position 2", image: ""},
+        {name: "position 3", image: ""}
+    ]
+
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.listen("3000", function(){
+    console.log("The YelpCamp server is listening on port 3000");
+});
