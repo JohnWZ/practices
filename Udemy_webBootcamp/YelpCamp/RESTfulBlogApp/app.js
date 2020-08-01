3a5e8ca085dfe5006dd3ae3380d075a939ece608
johnWZ <johnzhaiisgreat@gmail.com>
2020-08-01 10:47:53 +0930

    add edit and delete routes

--- Udemy_webBootcamp/YelpCamp/RESTfulBlogApp/app.js
+++ Udemy_webBootcamp/YelpCamp/RESTfulBlogApp/app.js
@@ -2,6 +2,8 @@
 
 var bodyParser = require ('body-parser'),
     mongoose = require('mongoose'),
+    methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer'),
     express = require('express'),
     app = express();
 
@@ -9,6 +11,9 @@ var bodyParser = require ('body-parser'),
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+// expressSanitizer should go after bodyParser
+app.use(expressSanitizer());
 
 // connect to the database (create one if doesn't exit)
 mongoose.connect("mongodb://localhost/restful_blog_app", {
@@ -55,13 +60,77 @@ app.get('/blogs', function(req, res){
     });
 });
 
-// NEW route
+// NEW route: show new blog form
 app.get('/blogs/new', function(req, res){
     res.render("new");
 });
 
-// CREATE route
+// CREATE route: create a new blog, then redirect somewhere
+app.post('/blogs', function(req, res){
+    // sanitize user input
+    // console.log(req.body); // before sanitize
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    // console.log(req.body); // after sanitize
+    // create a new blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if (err) {
+            console.log(err);
+            res.render('new');
+        } else {
+            // redirect to index
+            res.redirect('/blogs');
+        }
+    });
+});
 
+// SHOW route: show info about one specific blog
+app.get('/blogs/:id', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT route: show edit from for one blog
+app.get('/blogs/:id/edit', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    })
+});
+
+// UPDATE route: update particular dog, then redirect somewhere
+// Update和Show使用同一个URL，查看某个具体的blog的同时也可以修改它
+app.put('/blogs/:id', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// DELETE route: delete a particular dog, then redirect somewhere
+app.delete('/blogs/:id', function(req, res){
+    // destroy blog
+    // there is no returned blog in the params of the call back function
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            // redirect
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen("3000", function(){
     console.log("Servring on port 3000");
