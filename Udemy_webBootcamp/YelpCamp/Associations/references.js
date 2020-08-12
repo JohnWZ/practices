27d12449e8aa80472a6619a5fbe652d06f031d20
johnWZ <johnzhaiisgreat@gmail.com>
2020-08-12 23:30:21 +0930

    finished

--- Udemy_webBootcamp/YelpCamp/Associations/references.js
+++ Udemy_webBootcamp/YelpCamp/Associations/references.js
@@ -25,14 +25,29 @@ var userSchema = new mongoose.Schema({
 });
 var User = mongoose.model("User", userSchema);
 
+// create a new post, find the user and push the post to that user's posts
 // Post.create({
-//     title: "how to make it",
-//     content: "temp, temp"
+//     title: "how to make it Pt.3",
+//     content: "sand, sand, sand"
 // }, function(err, post){
 //     if (err) {
 //         console.log(err);
 //     } else {
-//         console.log(post);
+//         User.findOne({email: "john@123.com"}, function(err, foundUser){
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 foundUser.posts.push(post);
+//                 // save the user to the database
+//                 foundUser.save(function(err, data){
+//                     if (err) {
+//                         console.log(err);
+//                     } else {
+//                         console.log(data);
+//                     }
+//                 });
+//             }
+//         })
 //     }
 // });
 
@@ -47,3 +62,12 @@ var User = mongoose.model("User", userSchema);
 //     }
 // });
 
+// find a user and find all the posts for that user 
+// fill in the postId array will all the post data by populate(填充).exec()
+User.findOne({email:"john@123.com"}).populate("posts").exec(function(err, user) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(user);
+    }
+});
